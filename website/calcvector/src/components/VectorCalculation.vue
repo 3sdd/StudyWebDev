9ef0d4ea0fb0ec7592c18d09715893a8030c0ff7
3sdd <56486103+3sdd@users.noreply.github.com>
2020-05-12 21:46:14 +0900

    計算式の表示方法変更とベクトル描画
    - 計算式をKaTeXに変更
    - ベクトルを描画するようにした

--- website/calcvector/src/components/VectorCalculation.vue
+++ website/calcvector/src/components/VectorCalculation.vue
@@ -1,34 +1,20 @@
 <template>
     <div>
         <span>
-            <math>
-                <mover>
-                    <mi> a</mi>
-                    <mo>&rarr;</mo>
-                </mover>
-                <mo>+</mo>
-                <mover>
-                    <mi> b</mi>
-                    <mo>&rarr;</mo>
-                </mover>
-                <mo>=</mo>
-
-            </math>
+            <span v-html="getResultsHtml()"> </span>
         </span>
-        <span>
-            <span v-for="(vector,index) in vectors" v-bind:key="index" v-bind:vectors="vectors">
-                ({{vector.x}} , {{vector.y}}) <span v-if="index<vectorLength-1">+</span>
-            </span>
-            =
-            <span>( {{addtionResult.x}} , {{addtionResult.y}} )</span>
-        </span>
-
-        <!-- <span>({{vectors[0].x}},{{vectors[0].y}})+({{vectors[1].x}},{{vectors[1].y}})</span> -->
+        <div id="board" class="jxgbox"></div>
     </div>
 </template>
 
 <script>
 import {eventBus} from "../main"
+import katex from "katex"
+import "katex/dist/katex.min.css"
+// import renderMathInElement from "katex/dist/contrib/auto-render.js"
+import "jsxgraph/distrib/jsxgraph.css"
+import JXG from "jsxgraph/distrib/jsxgraphcore.js"
+
 export default {
     name:"VectorCalculation",
     data:function(){
@@ -36,18 +22,56 @@ export default {
             vectors:[
                 {vectorName:"a",x:0,y:0},
                 {vectorName:"b",x:0,y:0}
-            ]
+            ],
+            board:{},
         }
     },
     mounted:function(){
         const t=this;
         eventBus.$on("onVectorsUpdated",function(args){
             t.updateResult(args);
-        })
+            t.board.update();
+        });
+        this.initGraph();
     },
     methods:{
         updateResult:function(args){
             this.vectors=args.vectors;
+        },
+        // getVectorEquationText(x,y){
+        //     return katex.renderToString("c=\\pm\\sqrt{a^2+b^2}"+x+","+y,{
+        //         throwOnError:false
+        //     });
+            
+        // },
+        getVectorNameWithArrow(name){
+            return katex.renderToString(`\\vec{${name}}`,{
+                throwOnError:false
+            });
+        },
+        getResultsHtml(){
+            const veca=this.vectors[0];
+            const vecb=this.vectors[1];
+            const vecResult=this.getCalculationResult();
+            const text=`\\vec{a}+\\vec{b}=(${veca.x},${veca.y})+(${vecb.x},${vecb.y})=(${vecResult.x},${vecResult.y})`;
+            return katex.renderToString(text);
+        },
+        getCalculationResult(){
+            const x=this.vectors[0].x+this.vectors[1].x;
+            const y=this.vectors[1].y+this.vectors[1].y;
+            return {x:x,y:y};
+        },
+        initGraph(){
+            this.board=JXG.JSXGraph.initBoard("board", {boundingbox: [-10, 10, 10, -10], axis:true});
+            this.board.suspendUpdate();
+            
+            this.board.create("point",[()=>this.vectors[0].x,()=>this.vectors[0].y],{size:3,name:this.vectors[0].vectorName});
+            this.board.create("point",[()=>this.vectors[1].x,()=>this.vectors[1].y],{size:3,name:this.vectors[1].vectorName});
+            this.board.create("line",[[0,0],[()=>this.vectors[0].x,()=>this.vectors[0].y]],{straightFirst:false,straightLast:false,lastArrow:true});
+            this.board.create("line",[[0,0],[()=>this.vectors[1].x,()=>this.vectors[1].y]],{straightFirst:false,straightLast:false,lastArrow:true});
+
+            this.board.unsuspendUpdate();
+
         }
     },
     watch:{
@@ -68,11 +92,20 @@ export default {
                 result.y+=vec.y;
             }
             return result;
-        }
+        },
+
     }
 }
 </script>
 
 <style scoped>
+
+
+#board{
+    width: 500px;
+    height: 500px;
+    margin:auto;
+    margin-top:50px
+}
 
 </style>
