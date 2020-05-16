f27143dc916ad798fc116e0442204993c3bf184f
3sdd <56486103+3sdd@users.noreply.github.com>
2020-05-16 14:52:43 +0900

    treetext:vue-head加えて、meta情報など少し書いた

--- website/tree-text/src/components/global/PageHeader.vue
+++ website/tree-text/src/components/global/PageHeader.vue
@@ -1,16 +1,19 @@
 <template>
     <div>
         <b-navbar toggleable="lg" type="dark" variant="dark" class="p-3">
-            <b-navbar-brand href="#">Tree Text</b-navbar-brand>
+            <b-navbar-brand href="#" class="">Tree Text</b-navbar-brand>
 
             <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
-            <b-navbar-nav>
-                <b-nav-item href="#" >
-                    <font-awesome-icon :icon="['fab', 'twitter']"/>
+            <b-navbar-nav align="right" class="ml-auto">
+                <b-nav-item href="https://docs.google.com/forms/d/e/1FAIpQLSeWH6MO4pb-A_XabcOYu73VAmCufKZZNOSR9a1XE_fQBvlL1g/viewform?usp=sf_link">
+                    <font-awesome-icon icon="envelope"/>
                 </b-nav-item>
-                <b-nav-item href="#">
-                    <font-awesome-icon icon="bug"/>
+                <b-nav-item href="https://twitter.com/__msdd__" >
+                    <font-awesome-icon :icon="['fab', 'twitter']" size="lg"/>
+                </b-nav-item>
+                <b-nav-item href="https://docs.google.com/forms/d/e/1FAIpQLSdcnOL4F-IkaeCMwEJucK3yJW7ntOHxbg1_9u2XGzLKA1ZvIw/viewform?usp=sf_link">
+                    <font-awesome-icon icon="bug" size="lg"/>
                 </b-nav-item>
             </b-navbar-nav>
 
@@ -27,5 +30,14 @@ export default {
 }
 </script>
 
+
 <style scoped>
+.navbar-brand
+{
+  font-size: 25px;
+  font-weight: bold;
+}
+li.nav-item{
+    margin-right: 20px;
+}
 </style>
--- website/tree-text/src/main.js
+++ website/tree-text/src/main.js
@@ -5,20 +5,35 @@ import './plugins/bootstrap-vue'
 import App from './App.vue'
 import router from "./router.js"
 import {library} from "@fortawesome/fontawesome-svg-core"
-import {faCopy,faBug,faFileDownload} from "@fortawesome/free-solid-svg-icons"
+import {faCopy,faBug,faFileDownload,faEnvelope} from "@fortawesome/free-solid-svg-icons"
 import {faTwitter} from "@fortawesome/free-brands-svg-icons"
 import {FontAwesomeIcon} from "@fortawesome/vue-fontawesome"
 import VueClipboard from "vue-clipboard2"
+import VueHead from "vue-head"
 
-library.add(faCopy,faBug,faTwitter,faFileDownload);
+library.add(faCopy,faBug,faTwitter,faFileDownload,faEnvelope);
 Vue.component("font-awesome-icon",FontAwesomeIcon)
 
 Vue.use(VueClipboard)
+Vue.use(VueHead,{
+  separator: '-',
+  complement: 'Tree Text'
+})
 
 Vue.config.productionTip = false
 
+const head={
+  title:{
+    inner:"ホーム"
+  },
+  link:[
+    {rel:"author",href:"msdd",undo:false}
+  ]
+}
+
 new Vue({
   router,
+  head,
   render: h => h(App),
 }).$mount('#app')
 
