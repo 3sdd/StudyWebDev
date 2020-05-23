c860e146895dc0a600967d9ec3745877336037c9
3sdd <56486103+3sdd@users.noreply.github.com>
2020-05-23 15:37:27 +0900

    html2canvasを試した

---
+++ studylib/study-html2canvas/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <Html2Canvas />
+    <button>hi</button>
+  </div>
+</template>
+
+<script>
+import Html2Canvas from './components/Html2Canvas.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Html2Canvas
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ studylib/study-html2canvas/src/components/Html2Canvas.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <h1>hi</h1>
+    <div class="ss-area">
+      <span>ss-area</span>
+      <textarea rows="10" cols="50">
+      </textarea>
+    </div>
+    <button class="favorite styled"
+            type="button" @click="takeScreenshot">
+        SS保存
+    </button>
+  </div>
+</template>
+
+<script>
+import html2canvas from "html2canvas"
+
+export default {
+  name: 'Html2Canvas',
+  props: {
+
+  },
+  methods:{
+    takeScreenshot(){
+      console.log("ss")
+      html2canvas(document.querySelector(".ss-area"))
+        .then(function(canvas){
+
+            let link = document.createElement("a")
+            link.setAttribute('download', 'ss.png');
+            link.setAttribute('href', canvas.toDataURL("image/png").replace("image/png", "image/octet-stream"));
+            link.click();
+        });
+    }
+  }
+}
+</script>
+
+<style scoped>
+.ss-area{
+  width:200px;
+  margin:auto;
+  border:solid 1px red;
+}
+
+button{
+  padding:10px
+}
+</style>
