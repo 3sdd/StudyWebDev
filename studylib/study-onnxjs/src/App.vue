49258326ab3cdf771c1c46c9c15b6070c88bfbb0
3sdd <56486103+3sdd@users.noreply.github.com>
2020-05-24 23:15:56 +0900

    onnxjs 試したが、動かなかった

---
+++ studylib/study-onnxjs/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <onnx-test-component/>
+  </div>
+</template>
+
+<script>
+import OnnxTestComponent from "./components/OnnxTestComponent.vue"
+
+export default {
+  name: 'App',
+  components: {
+    OnnxTestComponent
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ studylib/study-onnxjs/src/components/OnnxTestComponent.vue
@@ -0,0 +1,68 @@
+<template>
+    <div>
+        hi
+        <span>{{outputTensor}}</span>
+    </div>
+</template>
+
+<script>
+import {InferenceSession} from "onnxjs"
+
+
+
+export default {
+    data:function(){
+        return {
+            outputTensor:"",
+        }
+    },
+    async mounted(){
+
+        const session=new InferenceSession({
+            backendHint:"cpu"
+        })
+        let path="";
+        if(process.env.NODE_ENV === 'production'){
+            path="/study-onnxjs/model.onnx";
+        }else{
+            path="/model(1).onnx";
+        }
+        console.log(path);
+        // this.modelFilepath = process.env.NODE_ENV === 'production' ? MODEL_FILEPATH_PROD : MODEL_FILEPATH_DEV;
+        session.loadModel(path)
+            .then(function(){
+                console.log("hi");
+
+            });
+        console.log("uu")
+        // const func=async()=>{
+
+
+            
+        //     const url="../static/model.onnx"
+        //     console.log([session,url,new Tensor()]);
+        //     // await session.loadModel(url);
+        //     console.log("model loaded")
+
+        //     // const inputs=[
+        //     //     new Tensor(new Float32Array([1.0,2.0,3.0,4.0]),"float32",[2,2])
+        //     // ]
+
+        //     // const outputMap=await session.run(inputs);
+        //     // const outputTensor=outputMap.values().next().value;
+        //     // console.log("return")
+        //     // return outputTensor;
+        // }
+
+        // // this.outputTensor=await func();
+        
+        // // console.log("end")
+        // await func();
+
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
